7b2a8a74c360b4879fe129bc27d348d80b3f738f
pikous <tomas.pikous@gmail.com>
2016-03-22 23:09:16 +0100

    added 2 new functions

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/pracujte/:name/:from', function (req, res) {
   res.send(req.params.name+" pracujte. <br>-"+req.params.from);
 });
 
+app.get('/kopac/:name/:from', function (req, res) {
+  res.send(req.params.name+" nebuďte kopáč. <br>-"+req.params.from);
+});
+
+app.get('/patent/:name/:count/:from', function (req, res) {
+  res.send(req.params.name+" mám "+req.params.name+" amerických patentů. <br>-"+req.params.from);
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
